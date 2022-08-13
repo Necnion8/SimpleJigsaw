af489fd983778f6c75c6f610d7e2786356666067
Necnion8 <necnionch@gmail.com>
2022-08-13 12:13:03 +0900

    first commit

---
+++ src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/SimpleJigsawPlugin.java
@@ -0,0 +1,17 @@
+package com.gmail.necnionch.myplugin.simplejigsaw.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleJigsawPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
