de7d6a308b994885833be685e6d244d74ba34453
Necnion8 <necnionch@gmail.com>
2022-08-13 12:21:58 +0900

    Add: WorldEditブリッジクラス

--- src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/SimpleJigsawPlugin.java
+++ src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/SimpleJigsawPlugin.java
@@ -1,17 +1,31 @@
 package com.gmail.necnionch.myplugin.simplejigsaw.bukkit;
 
+import com.gmail.necnionch.myplugin.simplejigsaw.bukkit.hooks.WorldEditBridge;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+import java.util.logging.Logger;
+
 public final class SimpleJigsawPlugin extends JavaPlugin {
+    private static SimpleJigsawPlugin instance;
+    private final WorldEditBridge worldEditBridge = new WorldEditBridge(this);
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
+
+    public static Logger getLog() {
+        return Objects.requireNonNull(instance, "Plugin is disabled").getLogger();
+    }
+
+    public static WorldEditBridge getWorldEdit() {
+        return Objects.requireNonNull(instance, "Plugin is disabled").worldEditBridge;
+    }
+
 }
---
+++ src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/hooks/WorldEditBridge.java
@@ -0,0 +1,18 @@
+package com.gmail.necnionch.myplugin.simplejigsaw.bukkit.hooks;
+
+import com.gmail.necnionch.myplugin.simplejigsaw.bukkit.SimpleJigsawPlugin;
+
+public class WorldEditBridge {
+
+    private final SimpleJigsawPlugin plugin;
+
+    public WorldEditBridge(SimpleJigsawPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+
+
+
+
+
+}
