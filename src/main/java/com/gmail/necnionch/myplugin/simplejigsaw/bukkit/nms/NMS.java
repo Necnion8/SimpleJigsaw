46f82e3c519cce814d623e43bd40093c7d0fa2b1
Necnion8 <necnionch@gmail.com>
2023-02-01 16:47:27 +0900

    Impl: 探検家の地図を任意の座標で作成するメソッド NMS.createExplorerMap() を追加

--- src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/nms/NMS.java
+++ src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/nms/NMS.java
@@ -1,10 +1,16 @@
 package com.gmail.necnionch.myplugin.simplejigsaw.bukkit.nms;
 
+import org.bukkit.Location;
+import org.bukkit.StructureType;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
 public interface NMS {
 
     @Nullable String getBiomeKeyByBlock(Block block);
+
+    @Nullable ItemStack createExplorerMap(World world, Location point, StructureType icon);
 
 }
--- v1_19_R2/src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/nms/v1_19_R2.java
+++ v1_19_R2/src/main/java/com/gmail/necnionch/myplugin/simplejigsaw/bukkit/nms/v1_19_R2.java
@@ -1,15 +1,25 @@
 package com.gmail.necnionch.myplugin.simplejigsaw.bukkit.nms;
 
+import net.minecraft.core.BlockPosition;
 import net.minecraft.core.IRegistry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.MinecraftKey;
+import net.minecraft.server.level.WorldServer;
+import net.minecraft.world.item.ItemWorldMap;
 import net.minecraft.world.level.biome.BiomeBase;
+import net.minecraft.world.level.saveddata.maps.MapIcon;
+import net.minecraft.world.level.saveddata.maps.WorldMap;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.StructureType;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_19_R2.CraftServer;
 import org.bukkit.craftbukkit.v1_19_R2.CraftWorld;
 import org.bukkit.craftbukkit.v1_19_R2.block.CraftBlock;
+import org.bukkit.craftbukkit.v1_19_R2.inventory.CraftItemStack;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.map.MapView;
 import org.jetbrains.annotations.Nullable;
 
 public class v1_19_R2 implements NMS {
@@ -28,6 +38,18 @@ public class v1_19_R2 implements NMS {
         MinecraftKey biomeKey = biomeRegistry.b(biomeBase);
 
         return biomeKey != null ? biomeKey.toString() : null;
+    }
+
+    @Nullable
+    @Override
+    public ItemStack createExplorerMap(World world, Location point, StructureType icon) {
+        WorldServer worldServer = ((CraftWorld)world).getHandle();
+        BlockPosition structurePosition = new BlockPosition(point.getBlockX(), point.getBlockY(), point.getBlockZ());
+        net.minecraft.world.item.ItemStack stack = ItemWorldMap.a(worldServer, structurePosition.u(), structurePosition.w(), MapView.Scale.NORMAL.getValue(), true, true);
+        ItemWorldMap.a(worldServer, stack);
+        ItemWorldMap.a(stack, worldServer);
+        WorldMap.a(stack, structurePosition, "+", MapIcon.Type.a(icon.getMapIcon().getValue()));
+        return CraftItemStack.asBukkitCopy(stack);
     }
 
 }
